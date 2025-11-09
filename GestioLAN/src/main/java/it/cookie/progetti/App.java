7446b6e89cdd289492bab902ebfb172a272b1af2
Marco <intensework443@gmail.com>
2025-11-09 15:11:43 +0100

    feat: First gui implementation
    Createt a window with a button for a small test using javaFX!

--- GestioLAN/src/main/java/it/cookie/progetti/App.java
+++ GestioLAN/src/main/java/it/cookie/progetti/App.java
@@ -1,13 +1,35 @@
 package it.cookie.progetti;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class App extends Application
 {
+    Button button;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        button = new Button("Click Me");
+        button.setOnAction(e -> System.out.println("Hello, JavaFX!"));
+
+        StackPane plane = new StackPane();
+        plane.getChildren().add(button);
+
+        Scene scene = new Scene(plane, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("GestioLAN Client");
+        primaryStage.show();
+    }
+
 }
