fbc686bdee3338488b303e2129837731f37cb2e4
Marco <intensework443@gmail.com>
2025-11-09 22:48:30 +0100

    Feat: Added the first FXML page tu the main

--- GestioLAN/src/main/java/it/cookie/progetti/App.java
+++ GestioLAN/src/main/java/it/cookie/progetti/App.java
@@ -4,15 +4,18 @@ package it.cookie.progetti;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Parent;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+// import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.util.ResourceBundle;
 import java.util.Locale;
+
 
 public class App extends Application implements EventHandler<ActionEvent>
 {
@@ -30,10 +33,13 @@ public class App extends Application implements EventHandler<ActionEvent>
         button = new Button(bundle.getString("button.login"));
         button.setOnAction(e -> System.out.println("Hello, JavaFX!"));
 
-        StackPane plane = new StackPane();
-        plane.getChildren().add(button);
+        //StackPane plane = new StackPane();
+        //plane.getChildren().add(button);
 
-        Scene scene = new Scene(plane, 300, 200);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/LoginView.fxml"), bundle);
+        Parent root = loader.load(); // Carica l'interfaccia dal file FXML
+
+        Scene scene = new Scene(root, 300, 200);
         primaryStage.setScene(scene);
         primaryStage.setTitle(bundle.getString("app.title"));
         primaryStage.show();
@@ -41,7 +47,7 @@ public class App extends Application implements EventHandler<ActionEvent>
 
     @Override
     public void handle(ActionEvent event) {
-        
+        System.out.println("Button clicked!");
         
     }
 }
