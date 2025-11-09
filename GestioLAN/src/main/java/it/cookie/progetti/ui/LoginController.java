5d5f9bc191d67a799676b0e6ec0f48e372cb9634
Marco <intensework443@gmail.com>
2025-11-09 22:49:29 +0100

    Feat: Created the base for the login page
    this is the "start" for the login page for the DB. (GUI Primaly)

---
+++ GestioLAN/src/main/java/it/cookie/progetti/ui/LoginController.java
@@ -0,0 +1,24 @@
+package it.cookie.progetti.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    
+
+    @FXML
+    private void handleLoginButtonAction() {
+        System.out.println("LoginController initialized");
+    }
+}
