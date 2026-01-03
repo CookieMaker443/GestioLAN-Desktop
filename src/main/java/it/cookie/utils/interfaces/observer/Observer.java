1c34e630919b350a7c1616436a7d0ac73e90f5c7
Marco <intensework443@gmail.com>
2026-01-03 03:09:34 +0100

    chore: added the obserer atternfiles

---
+++ src/main/java/it/cookie/utils/interfaces/observer/Observer.java
@@ -0,0 +1,5 @@
+package it.cookie.utils.interfaces.observer;
+
+public interface Observer {
+    public void Update(Subject subject, Object state);
+}
---
+++ src/main/java/it/cookie/utils/interfaces/observer/Subject.java
@@ -0,0 +1,22 @@
+package it.cookie.utils.interfaces.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+    private final List<Observer> observers = new ArrayList<>();
+
+    public void Attach(Observer o) {
+        observers.add(o);
+    }
+
+    public void Detach(Observer o) {
+        observers.remove(o);
+    }
+
+    public void Notify(Object state) {
+        for (Observer o : observers) {
+            o.Update(this, state);
+        }
+    }
+}
