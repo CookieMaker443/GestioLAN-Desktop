7efed9231d6bcdfb01465ecd6bbfa45bae19ff6f
Marco <intensework443@gmail.com>
2026-01-06 01:37:35 +0100

    feat: server ip settings

--- src/main/java/it/cookie/progetti/controllers/Login/Settings/ServerConfigController.java
+++ src/main/java/it/cookie/progetti/controllers/Login/Settings/ServerConfigController.java
@@ -1,5 +1,33 @@
 package it.cookie.progetti.controllers.Login.Settings;
 
-public class ServerConfigController {
+import it.cookie.utils.network.managers.NetworkManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
+public class ServerConfigController {
+    @FXML
+    private TextField ipField;
+
+    @FXML
+    private TextField portField;
+
+    @FXML
+    private Button connectButton;
+
+    @FXML
+    private void initialize() {
+        // Hint: initialize() will be called when the associated FXML has been completely loaded.
+        ipField.setText(NetworkManager.GetIstance().getIP()); // Default value
+        portField.setText(Integer.toString(NetworkManager.GetIstance().getPort()));    // Default value
+    }
+
+    @FXML
+    private void handleConnectButton() {
+        String ip = ipField.getText();
+        int port = Integer.parseInt(portField.getText());
+        NetworkManager.GetIstance().saveConfig(ip, port);
+    }
+
+    
 }
--- src/main/java/it/cookie/utils/network/managers/NetworkManager.java
+++ src/main/java/it/cookie/utils/network/managers/NetworkManager.java
@@ -1,13 +1,24 @@
 package it.cookie.utils.network.managers;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
 public class NetworkManager {
     String ip_addr;  
-    String port;
-    public NetworkManager istance;
+    int port;
 
-    private NetworkManager() {}
+    public static NetworkManager istance;
+    private Properties props = new Properties();
+    private final String CONFIG_PATH = "config/server.properties";
 
-    public NetworkManager GetIstance() {
+    private NetworkManager() {
+        loadConfig();
+    }
+
+    public static synchronized NetworkManager GetIstance() {
         if(istance == null) {
             istance = new NetworkManager();
         }
@@ -16,9 +27,48 @@ public class NetworkManager {
     
     public void ConnectToServer(String ip, String port) {}
 
-    public void Save() {}
+    public void saveConfig(String newIp, int newPort) {
+        this.ip_addr = newIp;
+        this.port = newPort;
+        
+        // Per salvare su file in modo persistente fuori dal JAR, 
+        // di solito si usa un percorso nel filesystem (es. cartella utente)
+        try (OutputStream output = new FileOutputStream("server.properties")) {
+            props.setProperty("server.ip", newIp);
+            props.setProperty("server.port", String.valueOf(newPort));
+            props.store(output, "Server Configuration");
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+
+    // Carica i dati dal file
+    public void loadConfig() {
+        // getResourceAsStream - il file è dentro il JAR/Risorse
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(CONFIG_PATH)) {
+            if (input == null) {
+                System.out.println("Configurazione non trovata, uso valori di default");
+                this.ip_addr = "localhost";
+                this.port = 8080;
+                return;
+            }
+            props.load(input);
+            this.ip_addr = props.getProperty("server.ip");
+            this.port = Integer.parseInt(props.getProperty("server.port"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public String GetBaseURL() {
         return "http://" + ip_addr + ":" + port;
+    }
+
+    public String getIP() {
+        return ip_addr;
+    }
+
+    public int getPort() {
+        return port;
     }
 }
